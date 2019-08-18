6cc0dfa19f0a5212417dc49f2f0f0f423f744835
Frederic Silber <freddy.silber@cloudcoach.com>
2019-08-18 05:35:45 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Freddy"
+    # resp.write "Hello, World."
+    # resp.write "Hello, World. You are in Freddy's World!"
+    # resp.write "This is a test."
     resp.finish
   end
 
 end
-
