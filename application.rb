37bfa378a638c70a3d262bf0aa82130ddf7d0353
Bernadette Davis <bsd231@nyu.edu>
2019-10-08 00:24:16 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,11 @@
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World\n"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
 end
-
